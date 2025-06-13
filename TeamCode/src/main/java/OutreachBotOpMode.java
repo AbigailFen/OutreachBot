14398f0706f09d728ad78b9387025b108e2e4171
bandi <fenrickabigail@gmail.com>
2025-06-12 20:21:29 -0500

    First Try

--- TeamCode/src/main/java/OutreachBotOpMode.java
+++ TeamCode/src/main/java/OutreachBotOpMode.java
@@ -10,7 +10,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-
+//Made by Abigail F
 @TeleOp(name="DOG", group="Linear OpMode")
 public class OutreachBotOpMode extends LinearOpMode {
 
